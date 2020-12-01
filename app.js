6bdd8c2b7798a66f912d3f67fba684750a8cccbd
MaiVanBinh <55869650+MaiVanBinh@users.noreply.github.com>
2020-12-01 23:33:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
+
+app.use(bodyParser.json());
 
 app.use(express.json());
 
@@ -32,7 +34,7 @@ app.get("/data/:token", (req, res) => {
   const data = JSON.parse(fs.readFileSync(dir + "/data.json", "utf-8"));
   res.status(200).json(data);
   // if (token === data.token) {
-    
+
   // } else {
   //   res.status(404).json({
   //     message: 'lol',
@@ -42,8 +44,9 @@ app.get("/data/:token", (req, res) => {
 
 app.post("/data", (req, res, next) => {
   const body = req.body;
+  console.log(body)
   fs.writeFileSync(dir + "/data.json", JSON.stringify(body));
-  res.status(201).json(body);
+  res.status(201).json("success");
 });
 
 app.listen(PORT, () => {
